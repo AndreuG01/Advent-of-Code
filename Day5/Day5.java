1f77dbc48371a669a684e2ff5816513899e55e25
AndreuG01 <andreu.garcies01@estudiant.upf.edu>
2021-12-07 08:11:30 +0100

    Day 5 is now more optimized

--- Day5/Day5.java
+++ Day5/Day5.java
@@ -3,30 +3,9 @@ package Day5;
 import java.util.*;
 import java.io.*;
 
-class Point {
-    private int xCoordinate;
-    private int yCoordinate;
-
-    public Point(int x, int y) {
-        xCoordinate = x;
-        yCoordinate = y;
-    }
-
-    public int getXCooridnate() { return xCoordinate; }
-    public int getYCoordinate() { return yCoordinate; }
-}
-
 public class Day5 {
-
-    static boolean pointIsInList(List<Point> list, Point point) {
-        for (int i = 0; i < list.size(); i++) {
-            Point currentPoint = list.get(i);
-            if (currentPoint.getXCooridnate() == point.getXCooridnate() && currentPoint.getYCoordinate() == point.getYCoordinate()) {
-                return true;
-            }
-        }
-        return false;
-    }
+    private static final int MAXSIZEDATA = 1000;
+    private static final int MAXSIZEEXAMPLE = 10;
 
     static int[] arrayNumLine(String line) {
         // The format of the line is "num1,num2 -> num3,num4"
@@ -44,9 +23,14 @@ public class Day5 {
         return numArray;
     }
 
-    static int partA(Scanner fileReader) {
-        List<Point> allPoints = new ArrayList<Point>();
-        List<Point> duplicatePoints = new ArrayList<Point>();
+    /**
+     * 
+     * @param fileReader the file from where we have to read the data
+     * @param maxSize the maximum size of the array. That is, the maximum x or y coordinate from the data file.
+     * @return the total points from horizontal or vertical lines that overlap
+     */
+    static int partA(Scanner fileReader, int maxSize) {
+        int[][] allPointsCounter = new int[maxSize][maxSize];
         int totalDuplicates = 0;
         while (fileReader.hasNext()) {
             // We extract the points that belong to an horiontal or vertical line
@@ -61,38 +45,37 @@ public class Day5 {
             if (x1 == x2) {
                 // Adding all the points in between
                 for (int i = Math.min(y1, y2); i <= Math.max(y1, y2); i++) {
-                    Point currentPoint = new Point(x1, i);
-                    if (pointIsInList(allPoints, currentPoint)) {
-                        if (pointIsInList(duplicatePoints, currentPoint) == false) {
-                            duplicatePoints.add(currentPoint);
-                            totalDuplicates += 1;
-                        }
-                    } else {
-                        allPoints.add(new Point(x1, i));
-                    }
+                    allPointsCounter[x1][i] += 1;
                 }
             } else if (y1 == y2) {
                 // Adding all the points in between
                 for (int i = Math.min(x1, x2); i <= Math.max(x1, x2); i++) {
-                    Point currentPoint = new Point(i, y1);
-                    if (pointIsInList(allPoints, currentPoint)) {
-                        if (pointIsInList(duplicatePoints, currentPoint) == false) {
-                            duplicatePoints.add(currentPoint);
-                            totalDuplicates += 1;
-                        }
-                    } else {
-                        allPoints.add(currentPoint);
-                    }
+                    allPointsCounter[i][y1] += 1;
                 }
             }
         }
+
+        // Counting the number of times where two or more points overlap
+        for (int i = 0; i < allPointsCounter.length; i++) {
+            for (int j = 0; j < allPointsCounter.length; j++) {
+                if (allPointsCounter[i][j] > 1) {
+                    totalDuplicates += 1;
+                }
+            }
+        }
+
         return totalDuplicates;
     }
 
-    static int partB(Scanner fileReader) {
+    /**
+     * 
+     * @param fileReader the file from where we have to read the data
+     * @param maxSize the maximum size of the array. That is, the maximum x or y coordinate from the data file.
+     * @return the total points from horizontal or vertical lines that overlap
+     */
+    static int partB(Scanner fileReader, int maxSize) {
         // We now need to consider diagonal lines as well.
-        List<Point> allPoints = new ArrayList<Point>();
-        List<Point> duplicatePoints = new ArrayList<Point>();
+        int[][] allPointsCounter = new int[maxSize][maxSize];
         int totalDuplicates = 0;
         while (fileReader.hasNext()) {
             // We extract the points that belong to an horiontal or vertical line
@@ -107,28 +90,12 @@ public class Day5 {
             if (x1 == x2) {
                 // Adding all the points in between
                 for (int i = Math.min(y1, y2); i <= Math.max(y1, y2); i++) {
-                    Point currentPoint = new Point(x1, i);
-                    if (pointIsInList(allPoints, currentPoint)) {
-                        if (pointIsInList(duplicatePoints, currentPoint) == false) {
-                            duplicatePoints.add(currentPoint);
-                            totalDuplicates += 1;
-                        }
-                    } else {
-                        allPoints.add(new Point(x1, i));
-                    }
+                    allPointsCounter[x1][i] += 1;
                 }
             } else if (y1 == y2) {
                 // Adding all the points in between
                 for (int i = Math.min(x1, x2); i <= Math.max(x1, x2); i++) {
-                    Point currentPoint = new Point(i, y1);
-                    if (pointIsInList(allPoints, currentPoint)) {
-                        if (pointIsInList(duplicatePoints, currentPoint) == false) {
-                            duplicatePoints.add(currentPoint);
-                            totalDuplicates += 1;
-                        }
-                    } else {
-                        allPoints.add(currentPoint);
-                    }
+                    allPointsCounter[i][y1] += 1;
                 }
             } else {
                 // Diagonal lines case
@@ -145,15 +112,7 @@ public class Day5 {
                     endY = y1;  
                 }
                 for (int i = startX; i <= endX; i++) {
-                    Point currentPoint = new Point(i, startY);
-                    if (pointIsInList(allPoints, currentPoint)) {
-                        if (pointIsInList(duplicatePoints, currentPoint) == false) {
-                            duplicatePoints.add(currentPoint);
-                            totalDuplicates += 1;
-                        }
-                    } else {
-                        allPoints.add(currentPoint);
-                    }
+                    allPointsCounter[i][startY] += 1;
                     if (startY < endY) {
                         startY += 1;
                     } else {
@@ -162,6 +121,16 @@ public class Day5 {
                 }
             }
         }
+
+        // Counting the number of times where two or more points overlap
+        for (int i = 0; i < allPointsCounter.length; i++) {
+            for (int j = 0; j < allPointsCounter.length; j++) {
+                if (allPointsCounter[i][j] > 1) {
+                    totalDuplicates += 1;
+                }
+            }
+        }
+
         return totalDuplicates;
     }
 
@@ -174,9 +143,9 @@ public class Day5 {
             
             
             // Data
-            System.out.println("Final result part a: " + partA(fileReaderA));
+            System.out.println("Final result part a: " + partA(fileReaderA, MAXSIZEDATA));
             fileReaderA.close();
-            System.out.println("Final result part b: " + partB(fileReaderB));
+            System.out.println("Final result part b: " + partB(fileReaderB, MAXSIZEDATA));
             fileReaderB.close();
 
         } catch (FileNotFoundException e) {
